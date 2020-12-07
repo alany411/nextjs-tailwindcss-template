5adbb30f05f95a9216064003406ade8064a31741
Alan Yang <alanyang411@gmail.com>
2020-12-07 11:21:06 -0500

    Update next and tailwindcss

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -31,13 +31,6 @@ export default class Document extends NextDocument {
     return (
       <Html lang='en'>
         <InlineStylesHead>
-          <link
-            rel='preload'
-            href='/fonts/Inter-roman.var-latin.woff2?3.13'
-            as='font'
-            type='font/woff2'
-            crossOrigin='true'
-          />
           <link rel='icon' href='/favicon.ico' />
         </InlineStylesHead>
         <body>
